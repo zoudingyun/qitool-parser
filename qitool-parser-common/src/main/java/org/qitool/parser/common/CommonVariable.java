dfcc63286d5c20f5db2d64d0b5c794cefd49fa45
zoudingyun <zoudingyun@foxmail.com>
2024-04-19 18:33:26 +0800

    修改配置

---
+++ qitool-parser-common/src/main/java/org/qitool/parser/common/CommonVariable.java
@@ -0,0 +1,16 @@
+package org.qitool.parser.common;
+
+/**
+ * 常用变量
+ *
+ * @author zoudingyun
+ * 2024/4/19 10:56
+ */
+public class CommonVariable {
+
+    /**
+     * 空字符串
+     * */
+    public static final String NULL_STRING = "";
+
+}
---
+++ qitool-parser-general/src/main/java/org/qitool/parser/general/BaseDataTypeUtil.java
@@ -0,0 +1,47 @@
+package org.qitool.parser.general;
+
+import java.math.BigInteger;
+
+import static org.qitool.parser.common.CommonVariable.*;
+
+/**
+ * 基础数据类型调整工具
+ *
+ * @author zoudingyun
+ * 2024/4/19 10:47
+ */
+public class BaseDataTypeUtil {
+
+    /**
+     * byte数据组转Hex字符串
+     * @param bytes byte数组
+     * @return 转换后的hex字符串
+     * */
+    public static String bytesToHexString(byte[] bytes) {
+        if (bytes == null || bytes.length == 0){
+            return NULL_STRING;
+        }else {
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                // 使用 String.format() 方法将字节转换为十六进制字符串，并在高位补零
+                sb.append(String.format("%02X", b & 0xFF));
+            }
+            return sb.toString();
+        }
+    }
+
+
+    /**
+     * byte数据组转Hex字符串
+     * @param hex hex字符串
+     * @return 转换后的byte数组
+     * */
+    public static byte[] hexStringToBytes(String hex) {
+        if (hex == null || hex.isEmpty()){
+            return (new byte[0]);
+        }else {
+            return new BigInteger(hex, 16).toByteArray();
+        }
+    }
+
+}
